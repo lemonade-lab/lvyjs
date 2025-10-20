693c169d4884061aa2ac221f1b61c6bbf2b08cae
ningmengchongshui <ningmengchongshui@gmail.com>
2025-10-21 00:46:16 +0800

    fix: windowsdev

--- packages/lvyjs/bin/index.js
+++ packages/lvyjs/bin/index.js
@@ -3,29 +3,18 @@
 import { fork } from 'child_process'
 import { join, dirname, relative } from 'path'
 import { fileURLToPath } from 'node:url'
-import { createRequire } from 'node:module'
 const args = [...process.argv.slice(2)]
 const currentFilePath = fileURLToPath(import.meta.url)
 const currentDirPath = dirname(currentFilePath)
 const pkgFilr = join(currentDirPath, '../package.json')
 const jsFile = join(currentDirPath, '../lib/index.js')
-const loaderFile = join(currentDirPath, '../lib/main.js')
 const jsdir = relative(process.cwd(), jsFile)
-const require = createRequire(import.meta.url)
-// const tsxDir = require.resolve('tsx/cli')
 // 构建时，使用 rollup
 if (args.includes('build')) {
   const argsx = args.filter(arg => arg !== 'build')
   const msg = fork(jsdir, ['--lvy-build', ...argsx], {
     stdio: 'inherit',
-    // 自己挂 execArgv
-    execArgv: [
-      ...process.execArgv,
-      '--require',
-      require.resolve('tsx/preflight'),
-      '--import',
-      require.resolve('tsx')
-    ],
+    execArgv: [...process.execArgv, '--require', 'tsx/preflight', '--import', 'tsx'],
     env: Object.assign({}, process.env, {
       PKG_DIR: pkgFilr
     }),
@@ -38,22 +27,16 @@ if (args.includes('build')) {
   // 运行时，使用 tsx
 } else if (args.includes('dev')) {
   const argsx = args.filter(arg => arg !== 'dev')
-  const argv = [jsdir, '--lvy-dev']
-  if (!args.includes('--no-watch')) {
-    argv.unshift('--clear-screen=false')
-    argv.unshift('watch')
-  }
-  const msg = fork(jsdir, [...argv, ...argsx], {
+  const msg = fork(jsdir, ['--lvy-dev', ...argsx], {
     stdio: 'inherit',
-    // 自己挂 execArgv
     execArgv: [
       ...process.execArgv,
       '--require',
-      require.resolve('tsx/preflight'),
+      'tsx/preflight',
       '--import',
-      require.resolve('tsx'),
+      'tsx',
       '--import',
-      loaderFile
+      'lvyjs/register'
     ],
     env: Object.assign({}, process.env, {
       PKG_DIR: pkgFilr
